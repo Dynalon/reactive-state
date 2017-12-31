db0b7edde080e903a7c3c15ba507a95886aafee5
Timo Dörr <timo@latecrew.de>
2017-12-31 12:35:03 +0100

    Fix devtool: Initial state and use newer enhancer name

--- src/devtool.ts
+++ src/devtool.ts
@@ -7,49 +7,70 @@ import { take } from "rxjs/operators/take"
 /* istanbul ignore next */
 
 // symbols only for debugging and devtools
-export { RootStateChangeNotification, StateChangeNotification } from './types'
+export { RootStateChangeNotification, StateChangeNotification } from "./types"
 
 export function enableDevTool<S extends object>(store: Store<S>) {
 
-    console.warn(" enableDevTool is work in progress; its not fully working and require the browser extension!");
+    console.warn("enableDevTool requires the browser extension. Note: the 'skip action' feature is not supported (but 'jump' works as expected')");
 
-    if (typeof window === "undefined" || !(window as any)["devToolsExtension"]) {
+    if (typeof window === "undefined") {
+        // nodejs deployments?
+        return;
+    }
+
+    const extension = (window as any)["__REDUX_DEVTOOLS_EXTENSION__"] || (window as any)["devToolsExtension"];
+    if (!extension) {
         console.warn("devToolsExtension not found in window (extension not installed?). Could not enable devTool");
         return;
     }
 
-    const devtoolExtension: StoreEnhancer<S> = (window as any)["devToolsExtension"]();
+    const devtoolExtension: StoreEnhancer<S> = extension();
     const reduxToReactiveSync = new Subject<S>();
     const reactiveStateUpdate = new Subject<any>();
 
-    // TODO get typing when s=> is undefined
     store.select(s => s, true).pipe(take(1)).subscribe(initialState => {
         let currentState: S = initialState;
 
-        const enhancer = (next: any) => {
-            return (reducer: any, initialState: any) => {
-                const store = next(reducer, initialState);
+        const enhancer: StoreEnhancer<S> = (next) => {
+            return (reducer, preloadedState) => {
+
+                // run any other store enhancers
+                const reduxStore = next(reducer, initialState);
 
                 // write back the state from DevTools/Redux to our ReactiveState
-                store.subscribe(() => {
-                    const reduxState = store.getState();
+                reduxStore.subscribe(() => {
+                    const reduxState = reduxStore.getState();
+                    console.info("Writing back redux state to reactive-state state: ", reduxState)
                     reduxToReactiveSync.next(reduxState);
                 })
 
                 reactiveStateUpdate.subscribe(p => {
                     currentState = p.state;
-                    store.dispatch({ type: p.actionName, payload: p.payload, state: p.state });
+                    reduxStore.dispatch({ type: p.actionName, payload: p.payload, state: p.state });
                 });
-                return store;
+                return reduxStore;
             };
         };
 
-        const reduxReducer = (state: S, action: ReduxAction & { state: any }) => {
+        // TODO: State should be type S, but TS does not yet support it
+        // maybe after TS 2.7: https://github.com/Microsoft/TypeScript/issues/10727
+        const reduxReducer = (state: any, action: ReduxAction & { state: any }) => {
+            // TODO: "skip" in devtools does not work here. In plain redux, we could call our reducers with the state
+            // and the action payload of the (replayed-) action. But we can"t to it with our store, as even if we
+            // could reset it to a state and replay the action, the operation is async. But we must return a state
+            // here in a sync manner... :(
+
+            if (action.type === "@@INIT") {
+                // redux internal action
+                return { ...state };
+            }
+            // What we actually do is instead of returning reduce(state, action) we return the result-state we have
+            // attached to action, that is kept in the log
             return { ...action.state };
         }
 
         createStore(
-            reduxReducer as any,
+            reduxReducer,
             initialState,
             compose(enhancer, devtoolExtension)
         );
@@ -59,7 +80,7 @@ export function enableDevTool<S extends object>(store: Store<S>) {
     notifyOnStateChange(store).subscribe(notification => {
         const { actionName, actionPayload, rootState } = notification;
         if (actionName !== "__INTERNAL_SYNC")
-            reactiveStateUpdate.next({ actionName, payload: actionPayload, state: rootState });
+            reactiveStateUpdate.next({ actionName: actionName || "UNNAMED", payload: actionPayload, state: rootState });
     })
 
     const syncReducer = (state: S, payload: any) => {
@@ -67,12 +88,4 @@ export function enableDevTool<S extends object>(store: Store<S>) {
     };
     store.addReducer(reduxToReactiveSync, syncReducer, "__INTERNAL_SYNC");
 };
-
-export function getNestedProperty(obj: object, keyChain: string[]) {
-    let current: any = obj;
-    keyChain.map(property => {
-        current = (obj as any)[property]
-    })
-    return current;
-}
 
