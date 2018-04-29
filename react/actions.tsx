2086c01ad5564d9a4744c944372b8b49531fa570
Timo Dörr <timo@latecrew.de>
2018-04-29 11:18:09 +0100

    Ultra-strict typing for actionmap to check function types and first argument

--- react/actions.tsx
+++ react/actions.tsx
@@ -1,10 +1,20 @@
 import { Observer, Observable } from 'rxjs';
 import { ExtractProps } from "./connect";
+
+// Taken from the TypeScript docs, allows to extract all functions of a type
+export type FunctionPropertyNames<T> = { [K in keyof T]: T[K] extends Function ? K : never }[keyof T];
+export type FunctionProperties<T> = Pick<T, FunctionPropertyNames<T>>;
+
+// Type that can be used to extract the first argument type of a function
+export type UnaryFunction<T> = (t: T, ...args: any[]) => any;
+
 // This will be a function that dispatches actions, but should not return anything
 export type ActionFunction = (...args: any[]) => any;
 
+// An ActionMap is a map with a list of properties, that are functions in the component props, and assigns these properties
+// either a ActionFunction or an Observer
 export type ActionMap<TComponentOrProps> = {
-    [P in keyof ExtractProps<TComponentOrProps>]?: ActionFunction | Observer<any>
+    [P in keyof FunctionProperties<ExtractProps<TComponentOrProps>>]?: ActionFunction | Observer<FunctionProperties<ExtractProps<TComponentOrProps>>[P] extends UnaryFunction<infer A> ? A : never>
 }
 
 /**
@@ -23,13 +33,13 @@ export type UnpackMap<TComponentState> = {
 export function assembleActionProps<TOriginalProps>(actionMap: ActionMap<TOriginalProps>): Partial<TOriginalProps> {
     const actionProps: any = {};
     for (let ownProp in actionMap) {
-        const field = actionMap[ownProp];
+        const field = (actionMap as any)[ownProp];
         const observerField = field as Observer<any>;
 
         if (field === undefined) continue;
 
         if (typeof field === "function") {
-            let func = actionMap[ownProp];
+            let func = (actionMap as any)[ownProp];
             actionProps[ownProp] = func;
         }
         // check if its an observable - TODO typeguard?
