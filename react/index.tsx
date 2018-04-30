877758a7eb455afafcd9d145f7927fd15c7438d7
Timo Dörr <timo@latecrew.de>
2018-04-30 14:04:20 +0100

    Remove state helpers that promoted bad pattern of usage

--- react/index.tsx
+++ react/index.tsx
@@ -1,18 +1,11 @@
 import { ActionFunction, ActionMap } from './actions';
 import { connect, MapStateToProps, ConnectResult } from './connect';
 import { StoreProvider } from './provider'
-// import { UnpackMap, mapToState, bindToState, unpackToState } from './state';
 
 export {
     // Action
     ActionFunction,
     ActionMap,
-
-    // State
-    // UnpackMap,
-    // unpackToState,
-    // mapToState,
-    // bindToState,
 
     // connect
     MapStateToProps,
