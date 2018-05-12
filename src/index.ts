8d90067d9366d1146a12d5273c89c335640c53ef
Timo Dörr <timo@latecrew.de>
2018-05-12 10:44:25 +0200

    Export shallowEqual

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Action } from "./action";
 import { Store } from "./store";
 import { Reducer, NamedObservable } from "./types";
+import { shallowEqual } from "./shallowEqual"
 export {
     Action,
     Store,
     Reducer,
-    NamedObservable
+    NamedObservable,
+    shallowEqual,
 }
